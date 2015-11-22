d99adaaee5034d78d6adba2dd23daf5b6bb359fd
Lucas Mahieu <lucas.mahieu@icloud.com>
2015-11-22 18:59:14 +0100

    Version qui marche pour tous les benchmark sauf le 06

--- src/computePatchOpt.c
+++ src/computePatchOpt.c
@@ -74,13 +74,13 @@ int computePatchOpt_it(int n, int m)
 	int i=0, j=0;
 	int L=0, ecrit=0;
 	int min = 0, cout=0;
-	char* toPrint = NULL; 
+	char* toPrint = (char*)malloc(sizeof(char)); 
 	char* tmpA = NULL;
 	size_t lA = 0;
 	char* tmpB = NULL;
 	size_t lB = 0;
-	unsigned int sumlBlen=0;
-	ssize_t lAlen=0, lBlen=0;
+	int k=0;
+	ssize_t lAlen=0, lBlen=200;
 	int cd = 10, cD=15, ca=0, cs=0;
 	int pi=0,pj=0;
 	int iD=0;
@@ -90,9 +90,7 @@ int computePatchOpt_it(int n, int m)
 	for(j = 0; j <=m; j++) {
 		if (j!=0) {
 			lBlen = getline(&tmpB, &lB, fB);
-			sumlBlen += 10 + lBlen;
 		}
-		toPrint = (char*)calloc(1+1+sizeof(int)+1+(lBlen+1)+1,sizeof(char));
 		rewind(fA);
 		iD=0;
 		for(i = 0; i <=n; i++) {
@@ -107,16 +105,22 @@ int computePatchOpt_it(int n, int m)
 				}
 				//i=0 et j!=0  -> f(0,j) = sum(10+LkB) avec k=1..j, calculé à chaque getline
 				else{
-					L = sumlBlen;
-						pi = i;
-						pj = j-1;
+					rewind(fB);
+					for(k=1; k<=j; k++){
+						lBlen = getline(&tmpB, &lB,fB);
+						L += 10 + lBlen;
+					}
+					pi = i;
+					pj = j-1;
+					free(toPrint);
+					toPrint = (char*)malloc((1+1+sizeof(int)+1+(strlen(tmpB)+1)+1)*sizeof(char));
 					if( (ecrit = sprintf(toPrint,"+ %d\n%s",i,tmpB))==-1){
-						printf(" Error ASPRINTF");
+						printf(" Error SPRINTF");
 						return EXIT_FAILURE;
 					}
 					//Maj du cout, de la cmd, et des peres pour retrouver le chemin
 					mem[0][j].cout = L; 
-					mem[0][j].cmd = (char*)calloc(ecrit,sizeof(char));
+					mem[0][j].cmd = (char*)malloc((strlen(toPrint)+1)*sizeof(char));
 					strcpy(mem[0][j].cmd, toPrint);
 					mem[0][j].pereI = pi; 
 					mem[0][j].pereJ = pj; 
@@ -129,6 +133,8 @@ int computePatchOpt_it(int n, int m)
 					// Si i=1, deletion coute 10
 					if (i==1) {
 						mem[i][0].cout = 10;
+						free(toPrint);
+						toPrint = (char*)malloc((1+1+sizeof(int)+1+1)*sizeof(char));
 						if ((ecrit = sprintf(toPrint,"d %d\n",i)) ==-1){
 							printf(" Error ASPRINTF");
 							return EXIT_FAILURE;
@@ -140,6 +146,8 @@ int computePatchOpt_it(int n, int m)
 					else{
 						// Si i>1, deletion coute 15
 						mem[i][0].cout = 15;
+						free(toPrint);
+						toPrint = (char*)malloc((1+1+2*sizeof(int)+1+1+1)*sizeof(char));
 						if( (ecrit = sprintf(toPrint,"D %d %d\n",1,i))==-1){
 							printf(" Error ASPRINTF");
 							return EXIT_FAILURE;
@@ -147,7 +155,7 @@ int computePatchOpt_it(int n, int m)
 						pi = i-i;
 						pj = j;
 					}
-					mem[i][0].cmd = (char*)calloc(ecrit,sizeof(char));
+					mem[i][0].cmd = (char*)calloc(strlen(toPrint)+1,sizeof(char));
 					strcpy(mem[i][0].cmd, toPrint);
 					mem[i][0].pereI = pi; 
 					mem[i][0].pereJ = pj; 
@@ -179,11 +187,15 @@ int computePatchOpt_it(int n, int m)
 					// On selectionne l'opération de cout min
 					min = sub;
 					if(cs == 0){
+						free(toPrint);
+						toPrint = (char*)malloc(1*sizeof(char));
 						ecrit = sprintf(toPrint,"");
 						pi = i-1;
 						pj = j-1;
 					}
 					else{
+						free(toPrint);
+						toPrint = (char*)malloc((1+1+sizeof(int)+1+strlen(tmpB)+1)*sizeof(char));
 						if((ecrit = sprintf(toPrint,"= %d\n%s",i,tmpB))==-1){
 							printf(" Error ASPRINTF");
 							return EXIT_FAILURE;
@@ -193,6 +205,8 @@ int computePatchOpt_it(int n, int m)
 					}
 					if( add<=min ){
 						min = add;
+						free(toPrint);
+						toPrint = (char*)malloc((1+1+sizeof(int)+1+strlen(tmpB)+1)*sizeof(char));
 						if((ecrit = sprintf(toPrint,"+ %d\n%s",i,tmpB))==-1){
 							printf(" Error ASPRINTF");
 							return EXIT_FAILURE;
@@ -202,6 +216,8 @@ int computePatchOpt_it(int n, int m)
 					}
 					if( del<min ){
 						min = del;
+						free(toPrint);
+						toPrint = (char*)malloc((1+1+sizeof(int)+1+1)*sizeof(char));
 						if((ecrit = sprintf(toPrint,"d %d\n",i))==-1){
 							printf(" Error ASPRINTF");
 							return EXIT_FAILURE;
@@ -211,6 +227,8 @@ int computePatchOpt_it(int n, int m)
 					}
 					if( Del<min ){
 						min = Del;
+						free(toPrint);
+						toPrint = (char*)malloc((1+1+2*sizeof(int)+1+1+1)*sizeof(char));
 						if((ecrit = sprintf(toPrint,"D %d %d\n",iD+1,i-iD))==-1){
 							printf(" Error ASPRINTF");
 							return EXIT_FAILURE;
@@ -220,7 +238,7 @@ int computePatchOpt_it(int n, int m)
 					}
 					// Maj du cout, de la cmd, et de l'opération qui a donnée ce min
 					mem[i][j].cout = min;
-					mem[i][j].cmd = (char*)calloc(ecrit,sizeof(char));
+					mem[i][j].cmd = (char*)calloc(strlen(toPrint)+1,sizeof(char));
 					strcpy(mem[i][j].cmd, toPrint);
 					mem[i][j].pereI = pi;
 					mem[i][j].pereJ = pj;
@@ -228,18 +246,16 @@ int computePatchOpt_it(int n, int m)
 					if(min<=mem[iD][j].cout) {
 						iD=i;
 					}
-					//if (toPrint != NULL){
-					//	free(toPrint);
-					//}
 				}
 			}
 			//free(tmpA);
-			lA=0;
+			//lA=0;
 		}
-		free(toPrint);
+		//free(toPrint);
 		//free(tmpB);
-		lB = 0;
+		//lB = 0;
 	}
+	free(toPrint);
 	i = i-1;
 	j = j-1;
 	cout = mem[i][j].cout;
@@ -385,7 +401,6 @@ int main ( int argc, char* argv[] )
 
 	//	printf(" 100%%\n");
 	//	printf("-------------- Patch écrit dans '%s'\n",argv[3]);
-	return 1;
 	for (i = n;  i>-1; i--) {
 		for (j = m;  j>-1; j--) {
 			free(mem[i][j].cmd);
